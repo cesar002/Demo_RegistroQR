44d638ac00721e56c18d98b6e17a2e4fe1278b85
Cesar <soul.unleashed13@gmail.com>
2018-11-02 00:24:35 -0600

    el mismo commit anterior

---
+++ Registro_APP/controller/QRImageGenerator.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const qr = require('qr-image');
+
+
+exports.generateQRCode = function(code) {
+
+    try{
+        let qr_png = qr.image(code, { type: 'png' })
+        let codename = `${code}.png`
+        qr_png.pipe(require('fs').createWriteStream(path.join(__dirname, '../public/codes', codename)));
+
+        return codename;
+    }catch (err) {
+        console.log('error al exportar imagen', err);
+
+        return null;
+    }
+    
+}
