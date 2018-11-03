773681c5c9995c112c3cd72a87cfe8e153739ebe
cesar002 <soul.unleashed13@gmail.com>
2018-11-03 14:01:55 -0600

    se agregÃ³ el lector de codigo QR y se agregÃ³ redux para manejar la informacion

--- Mobile-APP/App.js
+++ Mobile-APP/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import Main from './src/Navigator/index'
+import Reducers from './src/redux/index';
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+      this.setState({ isReady: true });
+  }
+
+
   render() {
+
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Provider store = { createStore(Reducers) } >
+            <Main/>
+        </Provider>
     );
+
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
